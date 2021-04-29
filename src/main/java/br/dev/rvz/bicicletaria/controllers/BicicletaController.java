7e3a7e5932944da6f0d88aaeb9de914da98602cd
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-29 18:52:11 -0300

    Criar controller de bicicleta

---
+++ src/main/java/br/dev/rvz/bicicletaria/controllers/BicicletaController.java
@@ -0,0 +1,9 @@
+package br.dev.rvz.bicicletaria.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("bicicletas/")
+public class BicicletaController {
+}
