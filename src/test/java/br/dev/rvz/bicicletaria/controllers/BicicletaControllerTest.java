b04095ea496f91ce95c8c1281fd1ef157acb7023
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-29 17:58:19 -0300

    Criar teste de controller

---
+++ src/test/java/br/dev/rvz/bicicletaria/controllers/BicicletaControllerTest.java
@@ -0,0 +1,10 @@
+package br.dev.rvz.bicicletaria.controllers;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
+public class BicicletaControllerTest {
+
+    
+}
