f3325511aba1f6f7215e56c13864f29b067e59d1
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-29 08:45:09 -0300

    Criar projeot

---
+++ src/main/java/br/dev/rvz/bicicletaria/BicicletariaApplication.java
@@ -0,0 +1,13 @@
+package br.dev.rvz.bicicletaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicicletariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicicletariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/rvz/bicicletaria/BicicletariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rvz.bicicletaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicicletariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
