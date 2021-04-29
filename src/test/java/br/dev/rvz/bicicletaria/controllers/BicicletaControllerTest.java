e934c7ee7296c41250353f3c6c4476e4ade7bc8f
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-29 18:24:27 -0300

    Criar teste de envio do json

--- src/test/java/br/dev/rvz/bicicletaria/controllers/BicicletaControllerTest.java
+++ src/test/java/br/dev/rvz/bicicletaria/controllers/BicicletaControllerTest.java
@@ -1,10 +1,50 @@
 package br.dev.rvz.bicicletaria.controllers;
 
 
+import br.dev.rvz.bicicletaria.domain.Bicicleta;
+import br.dev.rvz.bicicletaria.services.BicicletaService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @WebMvcTest
 public class BicicletaControllerTest {
 
-    
+    @MockBean
+    private BicicletaService bicicletaService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void testarCadastrarBicicletaComSucesso() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        CadastroBicicletaDTO cadastroBicicletaDTO = new CadastroBicicletaDTO("Modelo 1", "Tipo 1", 27);
+        String json = objectMapper.writeValueAsString(cadastroBicicletaDTO);
+
+        Bicicleta bicicleta = new Bicicleta(1, "Modelo 1", "Tipo 1", 27);
+
+        Mockito.when(bicicletaService.gravarBicicleta(Mockito.any(Bicicleta.class)))
+                .thenReturn(bicicleta);
+
+        String resultado = objectMapper.writeValueAsString(bicicleta);
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/bicicletas/")
+            .content(json)
+            .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().json(resultado));
+
+    }
 }
