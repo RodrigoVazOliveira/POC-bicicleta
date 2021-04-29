35b66677858a8236fa24de647632d5fc9be29cd5
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-29 18:54:49 -0300

    Criar método POST para cadastro de bicicleta

--- src/main/java/br/dev/rvz/bicicletaria/controllers/BicicletaController.java
+++ src/main/java/br/dev/rvz/bicicletaria/controllers/BicicletaController.java
@@ -1,9 +1,22 @@
 package br.dev.rvz.bicicletaria.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.dev.rvz.bicicletaria.domain.Bicicleta;
+import br.dev.rvz.bicicletaria.dtos.bicicleta.entrada.CadastroBicicletaDTO;
+import br.dev.rvz.bicicletaria.services.BicicletaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("bicicletas/")
 public class BicicletaController {
+
+    @Autowired
+    private BicicletaService bicicletaService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Bicicleta gravarNovaBicicleta(@RequestBody CadastroBicicletaDTO cadastroBicicletaDTO) {
+        
+    }
 }
